018f40b48d6a2180a9ae362c8dc2d37c939a3ee3
Simran Sinha <simransinha280@gmail.com>
2023-11-08 19:57:09 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!-- Footer page  -->
+<head>
+  <style>
+
+footer {
+    font-family: Montserrat, sans-serif;
+    background-color: #2d2d30;
+    padding: 10px;
+    }
+  </style>
+<body>
+  <br>
+  <br>
+  <br>
+ 
+<footer class="text-center">
+<div align="right"></div>
+ <a href="#" style="color: white; text-decoration: none"> <img src="https://img.icons8.com/color/60/000000/facebook.png"/></a>&nbsp&nbsp&nbsp
+   <a href="#" style="color: white; text-decoration: none"> <img src="https://img.icons8.com/color/60/000000/instagram-new.png"/></a>
+    <br>
+  <h3><img src="copy.ico" width="20px"><a style="color: white; text-decoration: none">&nbspPATNA-TICKET</a></h3>
+</div>
+</footer>
+
+</body></head>
